5ce2d090e5ceb5617a27e1e93fe2d3306ced2268
Cenk Kılıç <cenk@kilic.dev>
2021-03-21 14:55:09 +0100

    fix typescript rename function

--- lua/lsp-language-configurations/tsserver.lua
+++ lua/lsp-language-configurations/tsserver.lua
@@ -19,12 +19,27 @@ require('lspconfig').tsserver.setup {
         vim.call('inputsave')
 
         local current = vim.api.nvim_buf_get_name(0)
-        local rename = vim.call('input', 'Set the path to rename to > ', current)
+        local rename = vim.fn.input('Set the path to rename to' .. ' ➜ ', current)
 
-        print('\n')
-        print('Renaming:' .. current .. ' -> ' .. rename)
+        vim.api.nvim_command('normal :esc<CR>')
+
+        vim.api.nvim_out_write(current .. ' ➜ ' .. rename .. '\n')
 
         vim.lsp.buf.execute_command({command = '_typescript.applyRenameFile', arguments = {{sourceUri = 'file://' .. current, targetUri = 'file://' .. rename}}, title = ''})
+
+        vim.loop.fs_rename(current, rename)
+
+        for _, buf in pairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_is_loaded(buf) then
+            if vim.api.nvim_buf_get_name(buf) == current then
+              vim.api.nvim_buf_set_name(buf, rename)
+              -- to avoid the 'overwrite existing file' error message on write
+              vim.api.nvim_buf_call(buf, function()
+                vim.cmd('silent! w!')
+              end)
+            end
+          end
+        end
 
         vim.call('inputrestore')
       end
