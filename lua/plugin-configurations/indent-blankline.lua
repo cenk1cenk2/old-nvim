154b3de95636bb67ef76f959ca0df0b19ecfc19b
Cenk Kılıç <cenk@kilic.dev>
2021-03-23 20:10:05 +0100

    update indent blankline

--- lua/plugin-configurations/indent-blankline.lua
+++ lua/plugin-configurations/indent-blankline.lua
@@ -1,5 +1,7 @@
 vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_show_first_indent_level = false
 vim.g.indent_blankline_filetype_exclude = {'startify', 'far', 'gitcommit', 'terminal', 'floaterm'}
 vim.g.indent_blankline_enabled = true
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_context_highlight = 'Warning'
