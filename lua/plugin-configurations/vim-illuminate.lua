c5ce524b4be76c9ce2ac099c52a15dd801137844
Cenk Kılıç <cenk@kilic.dev>
2021-03-24 11:17:03 +0100

    refix illumunate

--- lua/plugin-configurations/vim-illuminate.lua
+++ lua/plugin-configurations/vim-illuminate.lua
@@ -1,2 +1,3 @@
-vim.api.nvim_set_keymap('n', '<a-e>', ':lua require("illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<a-r>', ':lua require("illuminate").next_reference{wrap=true}<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<a-e>', ':lua require("illuminate").next_reference{reverse=true,wrap=true}<cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<a-r>', ':lua require("illuminate").next_reference{wrap=true}<cr>', {noremap = true, silent = true})
+vim.g.Illuminate_delay = 50
