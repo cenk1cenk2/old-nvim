92b2021e3e3cdde5e067fff64162fd3916499c0e
Cenk Kılıç <cenk@kilic.dev>
2021-03-25 10:38:31 +0100

    disable formatting for eslint

--- lua/lsp-language-configurations/eslint.lua
+++ lua/lsp-language-configurations/eslint.lua
@@ -1,6 +1,7 @@
 local lspconfig = require 'lspconfig'
 local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
+local lsphelpers = require('helper-functions.lsp')
 
 -- Check if it's already defined for when reloading this file.
 if not lspconfig.eslint then
@@ -8,6 +9,9 @@ if not lspconfig.eslint then
     default_config = {
       cmd = {'node', vim.g.lsp_servers_dir .. 'eslint-language-server/eslintServer.js', '--stdio'},
       filetypes = {'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue'},
+      on_attach = function(client)
+        lsphelpers.disable_formatting(client)
+      end,
       root_dir = function(fname)
         return util.root_pattern('tsconfig.json')(fname) or util.root_pattern('package.json', 'jsconfig.json', '.git')(fname)
       end,
