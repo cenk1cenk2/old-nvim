9379659061770e3abae52c2ac85ebfc1152fb396
Cenk Kılıç <cenk@kilic.dev>
2021-04-07 13:55:30 +0200

    fix: fix keymaps for lsp

--- lua/lsp-configurations/lsp-config.lua
+++ lua/lsp-configurations/lsp-config.lua
@@ -4,9 +4,9 @@ vim.fn.sign_define('LspDiagnosticsSignInformation', {text = '', numhl = 'LspDiag
 vim.fn.sign_define('LspDiagnosticsSignHint', {text = '', numhl = 'LspDiagnosticsDefaultHint'})
 
 vim.api.nvim_set_keymap('n', 'gf', ':LspDefinition<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', 'gd', ':LspDeclaration<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', 'ge', ':LspDeclaration<CR>', {silent = true, noremap = true})
 vim.api.nvim_set_keymap('n', 'gr', ':LspReferences<CR>', {silent = true, noremap = true})
-vim.api.nvim_set_keymap('n', 'gi', ':LspImplemtation<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', 'gi', ':LspImplementation<CR>', {silent = true, noremap = true})
 vim.api.nvim_set_keymap('n', 'ca', ':Lspsaga code_action<CR>', {silent = true, noremap = true})
 vim.api.nvim_set_keymap('n', 'K', ':LspHover<CR>', {silent = true, noremap = true})
 vim.api.nvim_set_keymap('n', '<C-p>', ':LspGoToNext<CR>', {silent = true, noremap = true})
