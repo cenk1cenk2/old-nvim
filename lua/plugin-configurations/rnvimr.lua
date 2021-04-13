eeaa3ced4bb045da8ab6330229b92a562eba1b95
Cenk Kılıç <cenk@kilic.dev>
2021-04-13 22:33:52 +0200

    fix: update keybindings and ranger

--- lua/plugin-configurations/rnvimr.lua
+++ lua/plugin-configurations/rnvimr.lua
@@ -1,7 +1,7 @@
 vim.g.rnvimr_draw_border = 1
 vim.g.rnvimr_pick_enable = 1
 vim.g.rnvimr_bw_enable = 1
-vim.g.rnvimr_enable_ex = 1
+vim.g.rnvimr_enable_ex = 0
 vim.g.rnvimr_hide_gitignore = 0
 vim.g.rnvimr_ranger_cmd = 'ranger --cmd="set column_ratios 1,1"'
 vim.g.rnvimr_presets = {{width = 0.800, height = 0.800}}
