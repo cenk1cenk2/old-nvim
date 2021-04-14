33d1895ea1f6a5ef7748a6ce615e90e04033ae1f
Cenk Kılıç <cenk@kilic.dev>
2021-04-14 23:45:50 +0200

    fix: disable formatting for stylint

--- lua/lsp-language-configurations/stylelint.lua
+++ lua/lsp-language-configurations/stylelint.lua
@@ -1,2 +1,10 @@
 -- npm install -g stylelint-lsp
-require('lspconfig').stylelint_lsp.setup({cmd = {vim.g.lsp_servers_dir .. 'stylelint-lsp', '--stdio'}})
+local lsphelpers = require('helper-functions.lsp')
+
+require('lspconfig').stylelint_lsp.setup({
+  cmd = {vim.g.lsp_servers_dir .. 'stylelint-lsp', '--stdio'},
+
+  on_attach = function(client)
+    lsphelpers.disable_formatting(client)
+  end
+})
