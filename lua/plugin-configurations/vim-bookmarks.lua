15da18584eba38e7eec673ae1b960d6a6f7d0d04
Cenk Kılıç <cenk@kilic.dev>
2021-04-15 17:39:37 +0200

    fix: bookmark setup

--- lua/plugin-configurations/vim-bookmarks.lua
+++ lua/plugin-configurations/vim-bookmarks.lua
@@ -1,5 +1,6 @@
 vim.g.bookmark_no_default_key_mappings = 0
 vim.g.bookmark_sign = ''
+vim.g.bookmark_save_per_working_dir = 0
 vim.g.bookmark_auto_save = 1
-vim.g.bookmark_manage_per_buffer = 1
-vim.g.bookmark_auto_save_file = vim.env['HOME'] .. '.cache/nvim/bookmarks'
+vim.g.bookmark_manage_per_buffer = 0
+vim.g.bookmark_auto_save_file = vim.env['HOME'] .. '/.cache/nvim/bookmarks'
