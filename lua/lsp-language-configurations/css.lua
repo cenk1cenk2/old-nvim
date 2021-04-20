485ef3ab097c78984b50cdd1122b3f1a879e7244
Cenk Kılıç <cenk@kilic.dev>
2021-04-20 21:38:40 +0200

    fix: disable css for css for now

--- lua/lsp-language-configurations/css.lua
+++ lua/lsp-language-configurations/css.lua
@@ -1,5 +1,5 @@
 -- npm install -g vscode-css-languageserver-bin
 require('lspconfig').cssls.setup {
   cmd = {'node', vim.g.lsp_servers_dir .. 'vscode/css-language-features/server/dist/node/cssServerMain.js', '--stdio'},
-  filetypes = {'css', 'less', 'scss'}
+  filetypes = {'less', 'scss'}
 }
