ce20dfdf1f2b3a7876211424e0a8481c8a8ab746
Cenk Kılıç <cenk@kilic.dev>
2021-04-22 19:30:30 +0200

    fix: add ignore for lsptrouble

--- lua/plugin-configurations/better-whitespace.lua
+++ lua/plugin-configurations/better-whitespace.lua
@@ -1,4 +1,4 @@
 vim.g.better_whitespace_enabled = 1
-vim.g.better_whitespace_filetypes_blacklist = {'far', 'diff', 'gitcommit', 'help', 'dashboard', 'spectre_panel'}
+vim.g.better_whitespace_filetypes_blacklist = {'far', 'diff', 'gitcommit', 'help', 'dashboard', 'spectre_panel', 'LspTrouble'}
 vim.g.strip_whitespace_on_save = 1
 vim.g.strip_whitespace_confirm = 0
