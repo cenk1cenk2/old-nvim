4655cb621754ee6a17e1c3fe7ebff94948ca76b4
Cenk Kılıç <cenk@kilic.dev>
2021-04-23 10:56:39 +0200

    fix: add spectre to illumunate ignore

--- lua/plugin-configurations/vim-illuminate.lua
+++ lua/plugin-configurations/vim-illuminate.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_keymap('n', '<a-e>', ':lua require("illuminate").next_reference{reverse=true,wrap=true}<cr>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<a-r>', ':lua require("illuminate").next_reference{wrap=true}<cr>', {noremap = true, silent = true})
-vim.g.Illuminate_ftblacklist = {'floaterm', 'NvimTree'}
+vim.g.Illuminate_ftblacklist = {'floaterm', 'NvimTree', 'spectre_panel'}
 vim.g.Illuminate_delay = 50
