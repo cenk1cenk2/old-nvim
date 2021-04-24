79bb4292f07caf59447d61e8741cffa57e2680ce
Cenk Kılıç <cenk@kilic.dev>
2021-04-24 18:37:21 +0200

    fix: run prettier through node

--- lua/lsp-language-configurations/efm-configurations/prettier.lua
+++ lua/lsp-language-configurations/efm-configurations/prettier.lua
@@ -1,6 +1,6 @@
 -- return {formatCommand = ..vim.g.lsp_servers_dir .. 'prettierd ${INPUT}', formatStdin = true}
 return {
   -- formatCommand = 'prettier --config-precedence file-override --single-quote --print-width 180 --bracket-spacing --arrow-parens always --end-of-line lf --no-semi --stdin-filepath=${INPUT}',
-  formatCommand = 'PRETTIERD_DEFAULT_CONFIG=~/.config/nvim/utils/linter-config/.prettierrc.json ' .. vim.g.lsp_servers_dir .. 'prettierd ${INPUT}',
+  formatCommand = 'PRETTIERD_DEFAULT_CONFIG=~/.config/nvim/utils/linter-config/.prettierrc.json node ' .. vim.g.lsp_servers_dir .. 'prettierd ${INPUT}',
   formatStdin = true
 }
