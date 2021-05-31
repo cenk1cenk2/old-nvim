6dfbc4f2f3fa43a677f227f54a008584af1e6630
Cenk Kılıç <cenk@kilic.dev>
2021-05-31 13:17:50 +0200

    fix: disable self formatting for golang

--- lua/lsp-language-configurations/go.lua
+++ lua/lsp-language-configurations/go.lua
@@ -1,2 +1,11 @@
 -- https://github.com/golang/tools/tree/master/gopls
-require('lspconfig').gopls.setup {cmd = {vim.g.lsp_servers_dir .. 'gopls'}}
+local lsphelpers = require('helper-functions.lsp')
+
+require('lspconfig').gopls.setup({
+  cmd = {vim.g.lsp_servers_dir .. 'gopls'},
+  on_attach = function(client)
+    lsphelpers.disable_formatting(client)
+
+    lsphelpers.on_attach_illuminate(client)
+  end
+})
