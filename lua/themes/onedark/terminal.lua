7f9087ed998dd74ea6b9e2b947f372c4f445d65c
Cenk Kılıç <cenk@kilic.dev>
2021-07-19 11:42:54 +0200

    update terminal colors

--- lua/themes/onedark/terminal.lua
+++ lua/themes/onedark/terminal.lua
@@ -10,7 +10,7 @@ function M.setup()
   vim.g.terminal_color_5 = c.purple
   vim.g.terminal_color_6 = c.cyan
   vim.g.terminal_color_7 = c.fg
-  vim.g.terminal_color_8 = c.black
+  vim.g.terminal_color_8 = c.dark_grey
   vim.g.terminal_color_9 = c.red
   vim.g.terminal_color_10 = c.green
   vim.g.terminal_color_11 = c.yellow
