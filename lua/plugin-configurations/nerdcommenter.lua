e22ba60f856229bff87147310f6b14b1d53dca65
Cenk <cenk@kilic.dev>
2021-08-19 14:53:50 +0200

    fix: update commenting

--- lua/plugin-configurations/nerdcommenter.lua
+++ lua/plugin-configurations/nerdcommenter.lua
@@ -6,12 +6,10 @@ vim.g.NERDCommentEmptyLines = 1
 vim.g.NERDCompactSexyComs = 0
 
 -- comment with ctrl+#
-vim.api.nvim_set_keymap('n', [[<C-\>]], [[:call NERDComment(0,"toggle")<CR>]], {silent = true, noremap = true})
-vim.api.nvim_set_keymap('v', [[<C-\>]], [[:call NERDComment(0,"toggle")<CR>]], {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', [[<C-\>]], [[:call nerdcommenter#Comment(0,"toggle")<CR>]], {silent = true, noremap = true})
+vim.api.nvim_set_keymap('v', [[<C-\>]], [[:call nerdcommenter#Comment(0,"toggle")<CR>]], {silent = true, noremap = true})
 
-vim.api.nvim_set_keymap('n', [[<M-#>]], [[:call NERDComment(0,"sexy")<CR>]], {silent = true, noremap = true})
-vim.api.nvim_set_keymap('v', [[<M-#>]], [[:call NERDComment(0,"sexy")<CR>]], {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', [[<M-#>]], [[:call nerdcommenter#Comment(0,"sexy")<CR>]], {silent = true, noremap = true})
+vim.api.nvim_set_keymap('v', [[<M-#>]], [[:call nerdcommenter#Comment(0,"sexy")<CR>]], {silent = true, noremap = true})
 
-vim.api.nvim_exec([[
-  autocmd FileType javascript.jsx setlocal commentstring={/*\ %s\ */}
-]], true)
+vim.g.NERDCustomDelimiters = {typescript = {left = [[//]], leftAlt = [[/**]], rightAlt = [[*/]]}, javascript = {left = [[//]], leftAlt = [[/**]], rightAlt = [[*/]]}}
