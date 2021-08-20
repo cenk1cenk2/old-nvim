dcdef17144d8e2b24f1af5efc9d75c948dca6bd8
Cenk <cenk@kilic.dev>
2021-08-20 15:27:47 +0200

    fix: fix rooter patterns because it was going crazy

--- lua/plugin-configurations/vim-rooter.lua
+++ lua/plugin-configurations/vim-rooter.lua
@@ -1,1 +1,3 @@
-vim.g.rooter_silent_chdir = 1
+vim.g.rooter_silent_chdir = 0
+
+vim.g.rooter_patterns = {'.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'lerna.json', 'workspace.json', 'nx.json'}
