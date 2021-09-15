b201bbc54a76ac4f0d0b39eaa1518c3d1370982f
Cenk <cenk@kilic.dev>
2021-09-15 14:42:27 +0200

    fix: update option for scrolling

--- lua/plugin-configurations/neoscroll.lua
+++ lua/plugin-configurations/neoscroll.lua
@@ -3,6 +3,6 @@ require('neoscroll').setup({
   mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb'},
   hide_cursor = false, -- Hide cursor while scrolling
   stop_eof = true, -- Stop at <EOF> when scrolling downwards
-  respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+  respect_scrolloff = true, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
   cursor_scrolls_alone = false -- The cursor will keep on scrolling even if the window cannot scroll further
 })
