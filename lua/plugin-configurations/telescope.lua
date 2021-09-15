d61f3b2b2eb7533bee039a1fed7c775e1dc78a73
Cenk <cenk@kilic.dev>
2021-09-15 14:56:42 +0200

    chore: delete old function

--- lua/plugin-configurations/telescope.lua
+++ lua/plugin-configurations/telescope.lua
@@ -35,26 +35,6 @@ telescope.load_extension('gh')
 telescope.load_extension('lsp_handlers')
 telescope.load_extension('media_files')
 
--- local action_state = require('telescope.actions.state')
--- local actions = require('telescope.actions')
---
--- -- add close buffer
--- Telescope_custom_buffers = function()
---   require('telescope.builtin').buffers({
---     attach_mappings = function(prompt_bufnr, map)
---       local delete_buf = function()
---         local selection = action_state.get_selected_entry()
---         actions.close(prompt_bufnr)
---         vim.api.nvim_buf_delete(selection.bufnr, {force = true})
---       end
---
---       map('i', '<c-q>', delete_buf)
---
---       return true
---     end
---   })
--- end
---
 M = {}
 
 local TELESCOPE_RG_INTERACTIVE_LAST_ARGS
