1a8027944445ad6d660375cc64e7dee970764539
Cenk Kılıç <cenk@kilic.dev>
2021-09-18 19:14:09 +0200

    fix: neoscroll settings

--- lua/plugin-configurations/neoscroll.lua
+++ lua/plugin-configurations/neoscroll.lua
@@ -2,7 +2,7 @@ require('neoscroll').setup({
   -- All these keys will be mapped. Pass an empty table ({}) for no mappings
   mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb'},
   hide_cursor = false, -- Hide cursor while scrolling
-  stop_eof = true, -- Stop at <EOF> when scrolling downwards
-  respect_scrolloff = true, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  cursor_scrolls_alone = false -- The cursor will keep on scrolling even if the window cannot scroll further
+  stop_eof = false, -- Stop at <EOF> when scrolling downwards
+  respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+  cursor_scrolls_alone = true -- The cursor will keep on scrolling even if the window cannot scroll further
 })
