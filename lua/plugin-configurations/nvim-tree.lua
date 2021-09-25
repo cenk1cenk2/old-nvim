106a9794bc1d9cdd739ff19dac746d2fd28866af
Cenk Kılıç <cenk@kilic.dev>
2021-09-25 20:09:01 +0200

    fix: update plugin configuration

--- lua/plugin-configurations/nvim-tree.lua
+++ lua/plugin-configurations/nvim-tree.lua
@@ -1,13 +1,8 @@
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
 vim.g.nvim_tree_width = 40
-vim.g.nvim_tree_follow = 0
 vim.g.nvim_tree_hide_dotfiles = 0
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hijack_netrw = 1
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_auto_close = 0
-vim.g.nvim_tree_disable_netrw = 0
 vim.g.nvim_tree_auto_ignore_ft = {'startify', 'dashboard'}
 vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_bindings = {
@@ -42,3 +37,41 @@ vim.g.nvim_tree_bindings = {
 }
 
 vim.g.nvim_tree_icons = {['default'] = ''}
+
+require('nvim-tree').setup({
+  -- disables netrw completely
+  disable_netrw = true,
+  -- hijack netrw window on startup
+  hijack_netrw = true,
+  -- open the tree when running this setup function
+  open_on_setup = true,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup = {'dashboard'},
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close = false,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab = false,
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor = false,
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+  update_cwd = false,
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable = false,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd = nil,
+    -- the command arguments as a list
+    args = {}
+  }
+})
+
